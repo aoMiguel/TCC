5cd2287541aa1850d80a864194ae6449e21e41c4
aoMiguel <joaomiguel.msbj@gmail.com>
2024-09-13 11:17:22 -0300

    adicionando o banco

--- create-table.js
+++ create-table.js
@@ -1,74 +1,73 @@
-    import {sql} from './db.js' 
+import { sql } from './db.js';
 
 
-    sql`
-        CREATE TABLE Restaurante (
-            idRestaurante  TEXT PRIMARY KEY,
-            cnpj           VARCHAR(255),
-            nome           VARCHAR(255),
-            endereco       VARCHAR(255),
-            cep            VARCHAR(255),
-            cidade         VARCHAR(255)
-            bairro         VARCHAR(255),
-            num            INT,
-            compl          VARCHAR(255),
-            telefone       VARCHAR(20),
-            capacidade     INT
-            FOREIGN KEY (idRestaurante) REFERENCES Comanda(idRestaurante)
-        )'
-    `.then(()=>{
-        console.log("A tabla retaurante foi criada com sucesso")
-    },
-    ),
+        await sql`
+            CREATE TABLE Restaurante (
+                idRestaurante   TEXT PRIMARY KEY,
+                cnpj            VARCHAR(255),
+                nome            VARCHAR(255),
+                endereco        VARCHAR(255),
+                cep             VARCHAR(255),
+                cidade          VARCHAR(255),
+                bairro          VARCHAR(255),
+                num             INT,
+                compl           VARCHAR(255),
+                telefone        VARCHAR(20),
+                capacidade      INT
+            )
+        `;
+        console.log("A tabela Restaurante foi criada com sucesso");
 
-    sql`
-        CREATE TABLE Comanda (
-            usuarioid        TEXT PRIMARY KEY,
-            pratosid         TEXT PRIMARY KEY
-            idRestaurate     TEXT PRIMARY KEY
-            IDcomanda_num    PRIMARY KEY
-        )
+        await sql`
+            CREATE TABLE Pratos (
+                pratosid    TEXT PRIMARY KEY,
+                name        TEXT,
+                description TEXT,
+                price       DECIMAL(10, 2),
+                foto        TEXT
+            )
+        `;
+        console.log("A tabela Pratos foi criada com sucesso!");
+
+        await sql`
+            CREATE TABLE Comanda (
+                IDcomanda_num   TEXT PRIMARY KEY,
+                usuarioid       TEXT,
+                pratosid        TEXT,
+                idRestaurante   TEXT,
+                FOREIGN KEY (idRestaurante) REFERENCES Restaurante(idRestaurante),
+                FOREIGN KEY (pratosid) REFERENCES Pratos(pratosid)
+            )
+        `;
+        console.log("A tabela Comanda foi criada com sucesso");
+
+        await sql`
+            CREATE TABLE Cliente (
+                usuarioid        TEXT PRIMARY KEY,
+                idComanda        TEXT,
+                idRestaurante    TEXT,
+                gmail            VARCHAR(255),
+                whats            VARCHAR(20),
+                FOREIGN KEY (idRestaurante) REFERENCES Restaurante(idRestaurante),
+                FOREIGN KEY (idComanda) REFERENCES Comanda(IDcomanda_num)
+            )
+        `;
+        console.log('A tabela Cliente foi criada com sucesso!');
         
-    `.then(()=>{
-        
-    }),
-
-
-    sql`
-        CREATE TABLE pratos (
-            pratosid     TEXT PRIMARY KEY,
-            name         TEXT,
-            description  TEXT,
-            price        DECIMAL(10, 2),
-            foto         TEXT
-        )
-    `.then(()=>{
-        console.log("A tabela pratos foi criada com sucesso!")
-    }),
-
-    sql`
-        CREATE TABLE cliente (
-            usuarioid        TEXT PRIMARY KEY,
-            idComanda        VARCHAR(255),
-            idRestaurante    VARCHAR(255),
-            gmail            VARCHAR(255),
-            whats            VARCHAR(20)
-            FOREIGN KEY (idRestaurante) REFERENCES Comanda(idRestaurante),
-            FOREIGN KEY (IDcomanda_num) REFERENCES Comanda(IDcomanda_num)
-        )
-    `.then(()=>{
-        console.log('A tabela user foi criada com sucesso!')
-    }),
-
-    sql`
-        CREATE TABLE pedido (
-            pedidoID    TEXT PRIMARY KEY,
-            idpedido    TEXT,
-            datahora    TIMESTAMP,
-            idmesa      TEXT   
-        )
-    `.then(()=>{
-        console.log("A tabela pedidos foi criada com sucesso!")
-    })
-
-
+        await sql`
+            CREATE TABLE Pedido (
+                pedidoID         TEXT PRIMARY KEY,
+                quant            INT,
+                status           CHAR(1),
+                datapedid        TIMESTAMP,
+                valor_total      DECIMAL(10, 2),
+                desc_pedido      VARCHAR(255),
+                pratosid         TEXT,
+                id_comanda_num   TEXT,
+                idRestaurante    TEXT,
+                FOREIGN KEY (pratosid) REFERENCES Pratos(pratosid),
+                FOREIGN KEY (id_comanda_num) REFERENCES Comanda(IDcomanda_num),
+                FOREIGN KEY (idRestaurante) REFERENCES Restaurante(idRestaurante)
+            )
+        `;
+        console.log("A tabela Pedido foi criada com sucesso!");
--- database-postgres.js
+++ database-postgres.js
@@ -1,41 +1,51 @@
-import { randomUUID } from "node:crypto";
+import { randomUUID } from 'node:crypto';
 import { sql } from './db.js';
 
 export class DataBasePostgres {
+
    // Listar Pratos
    async listPratos(search) {
       if (search) {
-         return await sql`SELECT * FROM pratos WHERE name ILIKE ${'%' + search + '%'}`;
+         return await sql`SELECT * FROM Pratos WHERE name ILIKE ${'%' + search + '%'}`;
       } else {
-         return await sql`SELECT * FROM pratos`;
+         return await sql`SELECT * FROM Pratos`;
       }
    }
 
    // Listar Usuários
    async listUsuarios(search) {
       if (search) {
-         return await sql`SELECT * FROM usuario WHERE gmail ILIKE ${'%' + search + '%'}`;
+         return await sql`SELECT * FROM Cliente WHERE gmail ILIKE ${'%' + search + '%'}`;
       } else {
-         return await sql`SELECT * FROM usuario`;
+         return await sql`SELECT * FROM Cliente`;
       }
    }
 
    // Listar Pedidos
    async listPedidos(search) {
       if (search) {
-         return await sql`SELECT * FROM pedido WHERE idpedido ILIKE ${'%' + search + '%'}`;
+         return await sql`SELECT * FROM Pedido WHERE pedidoID ILIKE ${'%' + search + '%'}`;
       } else {
-         return await sql`SELECT * FROM pedido`;
+         return await sql`SELECT * FROM Pedido`;
+      }
+   }
+
+   // Listar Comandas
+   async listComanda(search) {
+      if (search) {
+         return await sql`SELECT * FROM Comanda WHERE IDcomanda_num ILIKE ${'%' + search + '%'}`;
+      } else {
+         return await sql`SELECT * FROM Comanda`;
       }
    }
 
    // Criar Prato
    async createPrato(prato) {
-      const pratoID = randomUUID(); // Gerar um ID único
+      const pratoID = randomUUID();
       const { name, foto, description, price } = prato;
 
       await sql`
-         INSERT INTO pratos (pratosid, name, foto, description, price) 
+         INSERT INTO Pratos (pratosid, name, foto, description, price) 
          VALUES (${pratoID}, ${name}, ${foto}, ${description}, ${price})
       `;
    }
@@ -45,7 +55,7 @@ export class DataBasePostgres {
       const { name, foto, description, price } = prato;
 
       await sql`
-         UPDATE pratos 
+         UPDATE Pratos 
          SET name = ${name}, foto = ${foto}, description = ${description}, price = ${price} 
          WHERE pratosid = ${id}
       `;
@@ -53,60 +63,86 @@ export class DataBasePostgres {
 
    // Deletar Prato
    async deletePrato(id) {
-      await sql`DELETE FROM pratos WHERE pratosid = ${id}`;
+      await sql`DELETE FROM Pratos WHERE pratosid = ${id}`;
    }
 
-   // Criar Usuário
-   async createUser(usuario) {
-      const usuarioID = randomUUID(); // Gerar um ID único
-      const { idComanda, idRestaurante, gmail, whats } = usuario;
+   // Criar Cliente
+   async createCliente(cliente) {
+      const usuarioID = randomUUID();
+      const { idComanda, idRestaurante, gmail, whats } = cliente;
 
       await sql`
-         INSERT INTO usuario (usuarioid, idComanda, idRestaurante, gmail, whats) 
+         INSERT INTO Cliente (usuarioid, idComanda, idRestaurante, gmail, whats) 
          VALUES (${usuarioID}, ${idComanda}, ${idRestaurante}, ${gmail}, ${whats})
       `;
    }
 
-   // Atualizar Usuário
-   async updateUser(id, usuario) {
-      const { idComanda, idRestaurante, gmail, whats } = usuario;
+   // Atualizar Cliente
+   async updateCliente(id, cliente) {
+      const { idComanda, idRestaurante, gmail, whats } = cliente;
 
       await sql`
-         UPDATE usuario 
+         UPDATE Cliente 
          SET idComanda = ${idComanda}, idRestaurante = ${idRestaurante}, gmail = ${gmail}, whats = ${whats} 
          WHERE usuarioid = ${id}
       `;
    }
 
-   // Deletar Usuário
-   async deleteUser(id) {
-      await sql`DELETE FROM usuario WHERE usuarioid = ${id}`;
+   // Deletar Cliente
+   async deleteCliente(id) {
+      await sql`DELETE FROM Cliente WHERE usuarioid = ${id}`;
    }
 
    // Criar Pedido
    async createPedido(pedido) {
-      const pedidoID = randomUUID(); // Gerar um ID único
-      const { idpedido, datahora, idmesa } = pedido;
+      const pedidoID = randomUUID();
+      const { quant, status, datapedid, valor_total, desc_pedido, pratosid, id_comanda_num, idRestaurante } = pedido;
 
       await sql`
-         INSERT INTO pedido (pedidoID, idpedido, datahora, idmesa) 
-         VALUES (${pedidoID}, ${idpedido}, ${datahora}, ${idmesa})
+         INSERT INTO Pedido (pedidoID, quant, status, datapedid, valor_total, desc_pedido, pratosid, id_comanda_num, idRestaurante) 
+         VALUES (${pedidoID}, ${quant}, ${status}, ${datapedid}, ${valor_total}, ${desc_pedido}, ${pratosid}, ${id_comanda_num}, ${idRestaurante})
       `;
    }
 
    // Atualizar Pedido
    async updatePedido(id, pedido) {
-      const { idpedido, datahora, idmesa } = pedido;
+      const { quant, status, datapedid, valor_total, desc_pedido, pratosid, id_comanda_num, idRestaurante } = pedido;
 
       await sql`
-         UPDATE pedido 
-         SET idpedido = ${idpedido}, datahora = ${datahora}, idmesa = ${idmesa}
+         UPDATE Pedido 
+         SET quant = ${quant}, status = ${status}, datapedid = ${datapedid}, valor_total = ${valor_total}, desc_pedido = ${desc_pedido}, pratosid = ${pratosid}, id_comanda_num = ${id_comanda_num}, idRestaurante = ${idRestaurante}
          WHERE pedidoID = ${id}
       `;
    }
 
    // Deletar Pedido
    async deletePedido(id) {
-      await sql`DELETE FROM pedido WHERE pedidoID = ${id}`;
+      await sql`DELETE FROM Pedido WHERE pedidoID = ${id}`;
+   }
+
+   // Criar Comanda
+   async createComanda(comanda) {
+      const { usuarioid, pratosid, idRestaurante, IDcomanda_num } = comanda;
+
+      await sql`
+         INSERT INTO Comanda (usuarioid, pratosid, idRestaurante, IDcomanda_num) 
+         VALUES (${usuarioid}, ${pratosid}, ${idRestaurante}, ${IDcomanda_num})
+      `;
+   }
+
+   // Atualizar Comanda
+   async updateComanda(id, comanda) {
+      const { usuarioid, pratosid, idRestaurante, IDcomanda_num } = comanda;
+
+      await sql`
+         UPDATE Comanda 
+         SET usuarioid = ${usuarioid}, pratosid = ${pratosid}, idRestaurante = ${idRestaurante}, IDcomanda_num = ${IDcomanda_num}
+         WHERE IDcomanda_num = ${id}
+      `;
+   }
+
+   // Deletar Comanda
+   async deleteComanda(id) {
+      await sql`DELETE FROM Comanda WHERE IDcomanda_num = ${id}`;
    }
 }
